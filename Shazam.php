d595d4f321a10741d9d3033614bad2e5465080b2
Andrew Martin <andy123@stanford.edu>
2019-01-15 10:27:58 -0800

    added flag to skip api endpoint for passing js url

--- Shazam.php
+++ Shazam.php
@@ -244,7 +244,13 @@ class Shazam extends \ExternalModules\AbstractExternalModule
             $this->emDebug("PAGE: " . PAGE);
             $this->emDebug("INSTRUMENT: ". $instrument);
 
-            $jsUrl = $this->getUrl('js/shazam.js', false, true);
+            $skipApi = $this->getProjectSetting("do-not-use-api-endpoint");
+
+            if ($skipApi) {
+                $jsUrl = $this->getUrl("js/shazam.js");
+            } else {
+                $jsUrl = $this->getUrl('js/shazam.js', false, true);
+            }
             // Highlight shazam fields on the page
             ?>
             <script src='<?php echo $jsUrl; ?>'></script>
@@ -252,7 +258,9 @@ class Shazam extends \ExternalModules\AbstractExternalModule
                 if (typeof Shazam === "undefined") {
                     alert("This page uses an external module called 'Shazam' but due to a configuration error " +
                         "the module is not loading the required javascript library correctly.\n\n" +
-                        "Please notify the project administrator."
+                        "You can try toggling the system-wide external module parameter to not use the api endpoint " +
+                        "and see if that makes a difference.\n\nPlease notify the project administrator.\n\n" +
+                        <?php echo $jsUrl ?>
                     );
                 } else {
                     Shazam.fields = <?php echo json_encode($this->shazam_instruments[$instrument]); ?>;
